aa8ea1dfa6d1ae4748bcfeae0a11157446e1e250
pwildenhain <pwildenhain@gmail.com>
2018-08-13 21:21:16 -0400

    Add update_current_balance()

--- models/budget.py
+++ models/budget.py
@@ -1,5 +1,6 @@
+# Import modules
 from datetime import datetime
-
+# Define classes
 class Account():
     """An account within a personal budget with the following properties:
 
@@ -18,8 +19,19 @@ class Account():
         self.current_balance = current_balance
         self.transaction_history = transaction_history
 
-    def add_transaction(self, comment, type, amount):
+    def update_current_balance(self, transaction_type, amount):
+        """When a transaction is recorded, update the current balance left on the account"""
+        if transaction_type == 'debit':
+            self.current_balance += -amount
+        elif transaction_type == 'credit':
+            self.current_balance += amount
+
+    def add_transaction(self, comment, transaction_type, amount):
+        """Record a transaction on the account transaction history and update the current balance"""
         date = datetime.now().strftime('%Y-%m-%d')
-        transaction = (date, self.name, comment, type, amount)
-        return self.transaction_history.append(transaction)
+        if type(amount) not in ['float', 'int']:
+            return "Amount entered must be a number'
+        self.update_current_balance(transaction_type, amount)
+        transaction = (date, self.name, comment, transaction_type, amount)
+        self.transaction_history.append(transaction)
 
