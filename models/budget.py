0de9010253f00f4bf9800bc31793b97ed65cfbe7
pwildenhain <pwildenhain@gmail.com>
2018-08-13 20:56:36 -0400

    Fix add_transaction()

--- models/budget.py
+++ models/budget.py
@@ -11,13 +11,14 @@ class Account():
         transaction_history:
     """
 
-    def __init__(self, name, category, current_balance, transaction_history):
+    def __init__(self, name, category, budgeted_amount, current_balance, transaction_history):
         self.name = name
         self.category = category
+        self.budgeted_amount = budgeted_amount
         self.current_balance = current_balance
         self.transaction_history = transaction_history
 
-    def add_transaction(self, date, comment, type, amount):
+    def add_transaction(self, comment, type, amount):
         date = datetime.now().strftime('%Y-%m-%d')
         transaction = (date, self.name, comment, type, amount)
         return self.transaction_history.append(transaction)
