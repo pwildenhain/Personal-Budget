04e7c7517c921fbe8b1375ed8e62d091820fc91e
pwildenhain <pwildenhain@gmail.com>
2018-08-16 23:16:14 -0400

    Add scripts for creation and updating of budget

---
+++ create-budget.py
@@ -0,0 +1,50 @@
+# Imports
+from models.budget import Account, Budget
+from sqlite3 import connect, OperationalError
+# Check for existing budget
+conn = connect('data/budget.db')
+cursor = conn.cursor()
+# If a table already exists, take note and ask if they want to overwrite
+try:
+    cursor.execute('SELECT * FROM budget_summary')
+except OperationalError:
+    budget_already_exists = False
+else:
+    budget_already_exists = True
+conn.close()
+
+if budget_already_exists:
+    print("Looks like a budget already exists")
+    overwrite_existing = ''
+    while overwrite_existing not in ['yes', 'no']:
+        overwrite_existing = input(
+            'Would you like to overwrite the existing budget? Type yes or no: '
+        ).lower() 
+    if overwrite_existing == 'no':
+        raise SystemExit
+# Initialize budget
+budget = Budget()
+print("Let's add some accounts to your personal budget")
+# Add accounts
+while True:
+    name = input("Name of this account: ")
+    category = input(f"What category does {name} fall under?: ")
+    while True:
+        try:
+            budgeted_amount = int(input(f"How much would you like to budget for {name}: "))
+        except ValueError:
+            print('Budgeted amount must be an integer')
+            continue
+        else:
+            break
+    budget.add_account(name = name, category = category, budgeted_amount = budgeted_amount)
+    # Ask for another
+    add_another = ''
+    while add_another not in ['yes', 'no']:
+        add_another = input('Would you like to add another account? Type yes or no: ').lower()
+    if add_another == 'yes':
+        continue
+    else:
+        break
+    
+
---
+++ update-budget.py
@@ -0,0 +1,110 @@
+# Imports
+from models.budget import Account, Budget
+from sqlite3 import connect
+from pandas import DataFrame, read_sql
+# Load data from SQLite budget_summary table
+conn = connect('data/budget.db')
+accounts = read_sql('SELECT * FROM budget_summary', conn)
+conn.close()
+accounts_dict = dict()
+for account in accounts.itertuples():
+    insert_account = Account(account.name, account.category, account.budgeted_amount)
+    accounts_dict[account.name] = insert_account
+budget = Budget(accounts_dict)
+# Different actions to update the budget
+while True:
+    print('What would you like to do?')
+    print('a) Add a transaction')
+    print('b) Add an account')
+    print('c) Update account budgeted amount')
+    print('d) Record a payday')
+    print('e) Exit the program')
+    action = input('Type a, b, c, d, or e: ').lower()
+    if action == 'a':
+        account = ''
+        while account not in budget.accounts.keys():
+            print(budget.display_accounts())
+            account = input('Choose one of the above accounts: ')
+        while True:
+            try:
+                amount = int(input('Transaction amount: '))
+            except ValueError:
+                print('Transaction amount must be an integer')
+                continue
+            else:
+                break
+        comment = input('Transaction comment: ')
+        budget.accounts[account].add_transaction(comment, 'debit', amount)
+        budget.display_summary()
+        continue
+    if action == 'b':
+        name = input("Name of this account: ")
+        category = input(f"What category does {name} fall under?: ")
+        while True:
+            try:
+                budgeted_amount = int(input(f"How much would you like to budget for {name}: "))
+            except ValueError:
+                print('Budgeted amount must be an integer')
+                continue
+            else:
+                break
+        budget.add_account(name = name, category = category, budgeted_amount = budgeted_amount)
+        budget.display_summary()
+        continue
+    if action == 'c':
+        account = ''
+        while account not in budget.accounts.keys():
+            print(budget.display_accounts())
+            account = input('Choose one of the above accounts: ')
+        current_budgeted_amount = budget.accounts[account].budgeted_amount
+        print(f'The current budgeted amount for {account} is {current_budgeted_amount}')
+        while True:
+            try:
+                new_budgeted_amount = int(input('Enter the new budgeted amount: '))
+            except ValueError:
+                print('New budgeted amount must be an integer')
+                continue
+            else:
+                budget.accounts[account].update_budgeted_amount(new_budgeted_amount)
+                update_comment = 'Update budgeted amount'
+                if new_budgeted_amount > current_budgeted_amount:
+                    budget.accounts[account].add_transaction(
+                        comment = update_comment,
+                        transaction_type = 'credit',
+                        amount = new_budgeted_amount - current_budgeted_amount
+                    )
+                elif current_budgeted_amount > new_budgeted_amount:
+                    budget.accounts[account].add_transaction(
+                        comment = update_comment,
+                        transaction_type = 'debit',
+                        amount = current_budgeted_amount - new_budgeted_amount
+                    )
+                else:
+                    print("You silly goose, those are the same numbers")
+                    continue
+                budget.display_summary()
+                break
+        continue
+    if action == 'd':
+        confirm = ''
+        while confirm not in ['yes', 'no']:
+            confirm = input(
+                'Are you sure you want to record a payday? Type yes or no: '
+            ).lower()
+        if confirm == 'yes':
+            print('$$$ *Cha-Ching* $$$')
+            budget.payday()
+            budget.display_summary()
+            continue
+        elif confirm == 'no':
+            continue
+    if action == 'e':
+        raise SystemExit
+    else:
+        continue
+
+
+
+        
+
+    
